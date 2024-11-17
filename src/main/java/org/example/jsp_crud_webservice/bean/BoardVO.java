3fc560f8f1030a64bade45fcc36a5e6c9fcdfafc
ehdgusqus <ehdgusqus55@gmail.com>
2024-11-17 20:14:48 +0900

    create BoardVo

---
+++ src/main/java/org/example/jsp_crud_webservice/bean/BoardVO.java
@@ -0,0 +1,60 @@
+package org.example.jsp_crud_webservice.bean;
+
+import java.util.Date;
+
+public class BoardVO {
+    private int id;
+    private String title;
+    private String content;
+    private String writer;
+    private int score;
+    private Date create_date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Date getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(Date create_date) {
+        this.create_date = create_date;
+    }
+}
